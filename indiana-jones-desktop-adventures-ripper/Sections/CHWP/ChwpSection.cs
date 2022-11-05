d90e13e624c1d54a2e41fe4e620339b273734031
Santeugini <dsanteugini@ea.com>
2022-11-05 18:50:47 +0100

    First iteration CHWP

--- indiana-jones-desktop-adventures-ripper/Sections/CHWP/ChwpSection.cs
+++ indiana-jones-desktop-adventures-ripper/Sections/CHWP/ChwpSection.cs
@@ -1,3 +1,4 @@
+using System;
 using indiana_jones_desktop_adventures_ripper.Models;
 using indiana_jones_desktop_adventures_ripper.Models.Base;
 
@@ -6,14 +7,26 @@ namespace indiana_jones_desktop_adventures_ripper.Sections.CHWP;
 public class ChwpSection : Section
 {
     public override string Tag => "CHWP";
+    private const int ChwpMetadataSize = 8;
     
     public override void Parse(DataBlock dataBlock)
     {
         base.Parse(dataBlock);
+
+        var unknown = Br.ReadBytes(ChwpMetadataSize);
+
+        var entries = 0;
         
         while (Ms.Position != dataBlock.Data.Length)
         {
-            Br.ReadBytes(dataBlock.Data.Length);
+            var unknown1 = Br.ReadInt16();
+            var unknown2 = Br.ReadInt16();
+            var unknown3 = Br.ReadInt16();
+            
+            Console.WriteLine($"unknown1: {unknown1} , unknown2: {unknown2}, unknown3 : {unknown3}");
+            entries++;
         }
+        
+        Console.WriteLine($"CHWP Entries: {entries}");
     }
 }
--- indiana-jones-desktop-adventures-ripper/Services/SectionService.cs
+++ indiana-jones-desktop-adventures-ripper/Services/SectionService.cs
@@ -69,6 +69,7 @@ namespace indiana_jones_desktop_adventures_ripper.Services
             RegisterDawSection(new PnamSection());
             RegisterDawSection(new AnamSection());
             */
+            RegisterDawSection(new ChwpSection());
             RegisterDawSection(new Puz2Section());
         }
 
